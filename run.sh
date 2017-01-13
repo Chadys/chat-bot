c46ead16c751aa4d35b322ad3b418f19688ce861
Chadys <chadys@hotmail.fr>
2017-01-13 16:10:16 +0100

    first commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#
+java -cp lib/Ab.jar Main bot=prog_assist action=chat trace=false
+
+
+
---
+++ update.sh
@@ -0,0 +1,7 @@
+#
+java -cp lib/Ab.jar Main bot=prog_assist action=aiml2csv trace=false
+java -cp lib/Ab.jar Main bot=prog_assist action=chat trace=false
+
+
+
+
